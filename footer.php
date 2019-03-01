a68f45eb5a07a2493bc761b1b22ee2f72a09459f
Juan Gallardo <jgallardo720@gmail.com>
2019-02-28 23:22:58 -0800

    first commit - completed first version of blog theme

---
+++ footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Maker
+ */
+
+?>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="wrap">
+			
+			<?php if ( has_nav_menu( 'footer' ) ) : ?>
+				<nav id="site-footer-navigation" class="footer-navigation" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'footer', 'depth' => 1, 'fallback_cb' => false ) ); ?>
+				</nav><!-- #site-navigation -->
+			<?php endif; ?>
+
+			<div class="site-info">
+				<span>&copy;</span> <?php echo date("Y"); ?>
+
+				<!-- Edit to your own footer text -->
+				Art blog by Juan Gallardo.
+			</div><!-- .site-info -->
+
+		</div><!-- .column -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
